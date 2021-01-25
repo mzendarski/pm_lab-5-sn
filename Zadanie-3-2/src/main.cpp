a2096f242c08caf04442107a7038942b183103d4
„Mikołaj <mikolaj.zendarski@student.wat.edu.pl>
2021-01-25 09:20:36 +0100

    Wykonano zadanie 3.2.1 – inicjalizacja LCD

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
+
+// Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7
+LiquidCrystal lcd(9, 8, 7, 6, 5, 4) ;
+int menu=1; //Numer aktualnie wyswietlanego ekranu
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(16,2);// Ustawienie kolumn i wierszy
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  dispMenu() ; //Wyswietlanie menu
+  delay(1000);
+  menu++; // Zmiana ekranu menu
+  if(menu>3) menu=1;
+}
+void dispMenu(void) {
+  switch(menu) {
+    case 1:
+        lcd.setCursor(0,0);
+        lcd.print("Menu 1");
+        break;
+    case 2:
+        lcd.setCursor(0,0);
+        lcd.print("Menu 2");
+    case 3:
+        lcd.setCursor(0,0);
+        lcd.print("Menu 3");
+        break;
+  }
 }
