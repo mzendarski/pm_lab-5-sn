3f5e5ae3a4ba215bf6cb7f92f2d4549163e9b439
„Mikołaj <mikolaj.zendarski@student.wat.edu.pl>
2021-01-25 09:43:33 +0100

    Wykonano zadanie 3.2.2 – sterowanie ekranami

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,20 +1,25 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
+#define buttonUp  11// Przycisk Następny - pin 11
+#define buttonDown  13// Przycisk Poprzedni - pin 13
+
 // Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4) ;
 int menu=1; //Numer aktualnie wyswietlanego ekranu
 
 void setup() {
-  lcd.begin(16,2);// Ustawienie kolumn i wierszy
+ lcd.begin(16,2); // Ustawianie liczby kolumn i wierszy
+// Konfiguracja wyprowadzeń mikrokontrolera
+pinMode (buttonUp, INPUT_PULLUP);
+pinMode (buttonDown, INPUT_PULLUP);
 }
 
-void loop() {
-  dispMenu() ; //Wyswietlanie menu
-  delay(1000);
-  menu++; // Zmiana ekranu menu
-  if(menu>3) menu=1;
+void loop(void) {
+    dispMenu(); // Wyswietlanie menu
+    changeMenu(); // Zmiana menu
 }
+
 void dispMenu(void) {
   switch(menu) {
     case 1:
@@ -29,4 +34,19 @@ void dispMenu(void) {
         lcd.print("Menu 3");
         break;
   }
+}
+
+void changeMenu(void) {
+   if(digitalRead(buttonUp) ==HIGH) { // Przycisk UP wciśnięty
+      menu=menu+1; // Następny ekran
+      if(menu>3){
+         menu=1;
+      }
+}
+if(digitalRead(buttonDown) == HIGH) { // Przycisk DOWN wcisniety
+   menu=menu-1; //Poprzedni ekran
+  if(menu<1) {
+     menu=3;
+  }
+ }
 }
